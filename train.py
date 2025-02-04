5f9004ffe885e796aa9b1a0cfc6497e499f6d4f1
AaronWangTW <honglin2023@gmail.com>
2025-02-03 21:09:21 -0600

    vectorized modification

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from gymnasium.envs.registration import register
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
 register(
     id="MotionProfilePacman-v1",
@@ -9,19 +10,21 @@ register(
     max_episode_steps=300,
 )
 
-env = make_vec_env("MotionProfilePacman-v1", n_envs=4, env_kwargs={"render_mode":"human"})
+if __name__=="__main__":
 
-model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="tensorboard")
-model.learn(total_timesteps=int(1e4), log_interval=4)
-model.save("ppo_pacbot")
+    env = make_vec_env("MotionProfilePacman-v1", n_envs=8, env_kwargs={"render_mode":"human"},vec_env_cls=SubprocVecEnv)
 
-del model  # remove to demonstrate saving and loading
+    model = PPO("MultiInputPolicy", env, device="cpu", verbose=1, tensorboard_log="tensorboard") #default policy is "MlpPolicy"
+    model.learn(total_timesteps=int(1e4), log_interval=4)
+    model.save("ppo_pacbot")
 
-model = PPO.load("ppo_pacbot")
+    del model  # remove to demonstrate saving and loading
 
-obs, info = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
+    model = PPO.load("ppo_pacbot")
+
+    obs, info = env.reset()
+    while True:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(action)
+        if terminated or truncated:
+            obs, info = env.reset()
